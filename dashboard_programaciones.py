eb50515e55ef6d19b5c9e05dae406d1690b123ce
analyrecaldep-hash <analyrecaldep@gmail.com>
2026-05-27 15:13:36 -0500

    Update dashboard_programaciones.py

--- dashboard_programaciones.py
+++ dashboard_programaciones.py
@@ -10,7 +10,7 @@ import plotly.express as px
 import streamlit as st
 
 st.set_page_config(
-    page_title="Dashboard de Seguimiento de Inscritos al Congreso",
+    page_title="Dashboard de Seguimiento de Inscritos al Congreso 2026",
     page_icon="📊",
     layout="wide",
 )
